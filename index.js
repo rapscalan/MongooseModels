31e1c416a274258176263ea9a86cbbcc1c6188da
Zak Hyslin <rapscalan@gmail.com>
2019-12-13 10:53:13 -0800

    travis

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-//const mongoose = require('mongoose');
-
-const Book = require('./lib/models/Book');
-//console.log(Book);
-
-// Book.create({
-//   title: 'Of Mice and Men',
-//   author: 'John Steinbeck',
-//   pages: 344,
-//   isbn: '2343hfer9823',
-//   genre: 'fiction'
-// }).then(() => console.log('created!!'));
 
